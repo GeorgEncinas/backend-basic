dfe2c3861ae6f94cade5733419553c5290db8e3c
jorge.encinas@gmail.com <jorge.encinas@gmail.com>
2020-06-18 23:23:58 -0400

    Initial commit

---
+++ src/sample.js
@@ -0,0 +1,39 @@
+const express = require("express")
+
+const sample = express()
+
+const array = [
+    "string",
+    false,
+    {
+        sub_id: 5
+    }
+]
+
+sample.get('/hello', function (req, res) {
+    res.status(200).json({
+        msg: 'world'
+    })
+})
+
+sample.get('/info/?id', function (req, res) {
+    const sub_id = req.query.subId
+    const echo = req.query.echo
+    const echo2 = req.query.echo2
+    const echo3 = req.query.echo3
+    const element = array[req.params.id]
+
+    res.status(200).json({
+        msg: 'ok',
+        element,
+        sub_id,
+        echo,
+        echo2,
+        echo3
+    })
+})
+
+const PORT = 8082
+sample.listen(PORT, function () {
+    console.log('listen at http://localhost:' + PORT) 
+})
