f29f7f295e45421e3bf4039b3d3d5469c924b701
jorge.encinas@gmail.com <jorge.encinas@gmail.com>
2020-06-22 22:45:03 -0400

    new tasks

--- src/task.js
+++ src/task.js
@@ -1,4 +1,8 @@
 // CRUD, investigar
-
+// https://sequelize.org/v5/class/lib/associations/base.js~Association.html, 
+// - BelongsTo: https://sequelize.org/v5/class/lib/associations/belongs-to.js~BelongsTo.html
+// - BelongsToMany: https://sequelize.org/v5/class/lib/associations/belongs-to-many.js~BelongsToMany.html
+// - HasMany: https://sequelize.org/v5/class/lib/associations/has-many.js~HasMany.html
+// - HasOne: https://sequelize.org/v5/class/lib/associations/has-one.js~HasOne.html
 // mongoose
 // sequelize
