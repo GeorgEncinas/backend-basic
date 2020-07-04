4075235d5e7c83c1f07917e1649b25ff63dd3e80
jorge.encinas@gmail.com <jorge.encinas@gmail.com>
2020-07-03 22:09:04 -0400

    fix token servise

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import express from 'express'
 import { userRoute } from "./routes/user";
 import { userRouteN } from "./routes/userN";
+import { verify } from "./services/tokenService";
+import cors from 'cors'
+import { sign } from 'jsonwebtoken';
 
 const sample = express()
+
+sample.use(cors({
+    origin: [
+        'http://localhost:4200',
+        'http://editor.swagger.io',
+        'https://editor.swagger.io'
+    ]
+}))
 
 sample.use(express.json()) // for parsing application/json
 sample.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -21,7 +32,27 @@ sample.get('/hello', (req, res) => {
     })
 })
 
-sample.get('/info/:id', (req, res) => {
+
+sample.get('/info/:id', async (req, res, next) => {
+    const token = req.get('X-Token')
+    if (token)
+        try {
+            const decoded = await verify(token)
+            console.dir({ decoded }, { colors: true })
+            next()
+        } catch (err) {
+            console.log('token catch> ', token)
+            if (err.name === 'TokenExpiredError')
+                res.status(401).json({ msg: err.message })
+            else
+                res.status(401).json({ msg: 'Invalid token' })
+            next(false)
+        }
+    else {
+        res.send(401).json({ msg: 'Invalid token' })
+        next(false)
+    }
+}, (req, res) => {
     const { subId } = req.query
     const echo = req.query.echo
     const echo2 = req.query.echo2
